3393201167af5bcb30a4cb47eca38005e966cfbd
Luigi Langella <luigi76langella@gmail.com>
2019-06-28 07:06:11 +0000

    initial commit

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+//will assign css class .selection to the odd numbered table rows
+ $(document).ready(function(){
+    $("tr").addClass("selection");
+    $('td').addClass('selection');
+  });
+  
+ $(document).ready(function(){
+    $('table').addClass('table-style');
+ });
